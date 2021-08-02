1868172ffe40c3a97ccc739301429a1db587ab76
aledguedes <alexandredguedes@gmail.com>
2021-08-01 23:47:13 -0300

    telas: login, recuperar senha, e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './users/cadastro/cadastro.component';
+import { IndexComponent } from './users/index/index.component';
+import { LoginComponent } from './users/login/login.component';
+import { RecuperarsenhaComponent } from './users/recuperarsenha/recuperarsenha.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'recuperarsenha', component: RecuperarsenhaComponent },
+  { path: 'index', component: IndexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms' 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './users/login/login.component';
+import { CadastroComponent } from './users/cadastro/cadastro.component';
+import { RecuperarsenhaComponent } from './users/recuperarsenha/recuperarsenha.component';
+import { IndexComponent } from './users/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    RecuperarsenhaComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
